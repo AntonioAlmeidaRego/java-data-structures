52a02cd5c7c9a83f66f652989183635d07e34f3d
Antonio <antonio.alm_pdf@hotmail.com>
2020-03-02 23:32:40 -0300

    Queue (Fila) duplamente encadeada circular in Java OOP

---
+++ java-data-structures/src/br/com/java/data/structures/QueueDataStructure.java
@@ -0,0 +1,73 @@
+package br.com.java.data.structures;
+
+public class QueueDataStructure<T extends Object> implements DataStructures<T> {
+	private Node<T> head = null;
+	private Node<T> syrup = null;
+	private int length = 0;
+	
+	public QueueDataStructure() {
+		head = new Node<T>();
+		syrup = new Node<T>();
+		
+		head.setProx(syrup);
+		head.setAnt(syrup);
+		syrup.setProx(head);
+		syrup.setAnt(head);
+	}
+	
+	@Override
+	public void add(T value) {
+		Node<T> novo = new Node<T>();
+		novo.setValue(value);
+		length++;
+		
+		if(isEmpty()) {
+			head.setProx(novo);
+			novo.setAnt(head);
+			novo.setProx(syrup);
+			syrup.setAnt(novo);
+		}else {
+			Node<T> aux = head.getProx();
+			novo.setAnt(head);
+			novo.setProx(aux);
+			aux.setAnt(novo);
+			head.setProx(novo);
+		}
+	}
+
+	@Override
+	public void remove() {
+		Node<T> aux = head.getProx();
+		Node<T> aux2 = aux.getProx();
+		head.setProx(aux2);
+		aux2.setAnt(head);
+		length--;
+	}
+	
+	@Override
+	public String toString() {
+		String str = "";
+		Node<T> aux = head.getProx();
+		
+		while(aux != syrup) {
+			str += aux.getValue() + " ";
+			aux = aux.getProx();
+		}
+		
+		return str;
+	}
+
+	@Override
+	public int size() {
+		return length;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if(head.getProx() == syrup) {
+			return true;
+		}
+		return false;
+	}
+
+}
--- java-data-structures/src/br/com/java/data/structures/main/Main.java
+++ java-data-structures/src/br/com/java/data/structures/main/Main.java
@@ -1,10 +1,12 @@
 package br.com.java.data.structures.main;
 
+import br.com.java.data.structures.QueueDataStructure;
 import br.com.java.data.structures.StackDataStructure;
 
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("----- STACK (PILHA) ------");
 		StackDataStructure<Integer> pilha = new StackDataStructure<Integer>();
 		pilha.add(25);
 		pilha.add(65);
@@ -12,6 +14,18 @@ public class Main {
 		pilha.add(14);
 		pilha.add(30);  
 		System.out.println(pilha);
+		
+		System.out.println("----- QUEUE (FILA) ------");
+		QueueDataStructure<Integer> fila = new QueueDataStructure<Integer>();
+		
+		fila.add(41);
+		fila.add(28);
+		fila.add(74);
+		fila.add(89);
+		fila.add(14);
+		
+		System.out.println(fila);
+		
 	}
 
 }
